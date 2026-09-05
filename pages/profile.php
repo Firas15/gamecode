ffd5009dd510b932166ea8ab661346a2c48862b5
Пользователь <livefiras89@gmail.com>
2026-09-05 21:31:10 +0300

    Отображение ITGorky

--- pages/profile.php
+++ pages/profile.php
@@ -207,6 +207,10 @@ $regDate  = date('d.m.Y', strtotime($user['created_at']));
     <button class="sidebar-close" id="sidebarClose">х</button>
     <div class="sidebar-logo">
       <span class="pixel-text">GAME</span><span class="pixel-text accent">CODE</span>
+      <a class="sidebar-partner" href="https://itgorky.ru/" target="_blank" rel="noopener">
+        <span class="partner-x">×</span>
+        <span class="partner-name">IT<em>GORKY</em></span>
+      </a>
     </div>
     <ul class="sidebar-nav">
       <li><a href="../index.html"    class="sidebar-link" >Все игры</a></li>
@@ -215,10 +219,6 @@ $regDate  = date('d.m.Y', strtotime($user['created_at']));
       <li><a href="theory.html"      class="sidebar-link" >Теория</a></li>
       <li><a href="about.html"       class="sidebar-link" >О нас</a></li>
     </ul>
-      <a class="sidebar-partner" href="https://itgorky.ru/" target="_blank" rel="noopener">
-        <span class="partner-x">×</span>
-        <span class="partner-name">IT<em>GORKY</em></span>
-      </a>
     <div class="sidebar-footer">
       <span>V2.0 — GameCode</span>
     </div>
